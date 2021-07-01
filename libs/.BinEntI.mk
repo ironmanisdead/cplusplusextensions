ae07729f2aa6f20f663f172028e47d94c752e4b5
ironmanisdead <mystuff20041@gmail.com>
2021-07-01 18:22:05 -0500

    removed .mk files again

--- libs/.BinEntI.mk
+++
@@ -1,4 +0,0 @@
-.BinEntI.o: BinEntI.cpp headers/BinEntI.hpp headers/Macros.hpp \
- headers/BinTree.hpp headers/Utils.hpp headers/Find.hpp
-	$(if $(wildcard .BinEntI.mk.2),rm .BinEntI.mk.2,.extra/depgen BinEntI.cpp)
-	gcc -std=c++2a -o .BinEntI.o -O3 -Wall -Wextra -c -fPIC BinEntI.cpp
--- libs/.Utils.mk
+++
@@ -1,3 +0,0 @@
-.Utils.o: Utils.cpp headers/Utils.hpp headers/Macros.hpp
-	$(if $(wildcard .Utils.mk.2),rm .Utils.mk.2,.extra/depgen Utils.cpp)
-	gcc -std=c++2a -o .Utils.o -O3 -Wall -Wextra -c -fPIC Utils.cpp
