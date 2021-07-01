c724996796e28aad2b143a7d0d5d58ea56fcf2e2
ironmanisdead <mystuff20041@gmail.com>
2021-07-01 13:39:11 -0500

    deleting several pre-made files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cplusplusextensions
 A new take on c++ extensions with extra flexibility
+
+PREREQUISITES:
+A machine that runs Unix or Linux
+gcc 8 (gcc 9 or greater preferred but definitely not nessecary)
+make (this is how it installs)
--- libs/.UVector.mk
+++
@@ -1,5 +0,0 @@
-.UVector.o: UVector.cpp headers/UVector.hpp headers/Utils.hpp \
- headers/Macros.hpp headers/Array.hpp headers/GString.hpp \
- headers/Tuple.hpp headers/String.hpp
-	$(if $(wildcard .UVector.mk.2),rm .UVector.mk.2,.extra/depgen UVector.cpp)
-	gcc -std=c++2a -o .UVector.o -O3 -Wall -Wextra -c -fPIC UVector.cpp
