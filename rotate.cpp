f146f2caa2996e49c86c18a57d78132d21e02c0f
Colton Jensen <mystuff20041@gmail.com>
2021-07-03 12:34:10 -0500

    updated rotate.cpp

--- rotate.cpp
+++ rotate.cpp
@@ -7,6 +7,6 @@ int main() {
 	map.insert(2, 1);
 	map.insert(1, 2);
 	map.insert(3, 5);
-	std::cout << map.search(1)->rotate(false) << std::endl;
+	std::cout << map.search(2)->rotate(false) << std::endl;
 	std::cout << map[1] << ' ' << map[2] << ' ' << map[3] << std::endl;
 }
