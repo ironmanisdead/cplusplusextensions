46a9372e5129f36173bd2af1b6c08c9675b9cd9e
Colton Jensen <mystuff20041@gmail.com>
2021-07-05 19:54:20 -0500

    added .gitignore back and modified install slightly

--- libs/headers/Vector.hpp
+++ libs/headers/Vector.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "UVector.hpp"
-//TODO: finish Vector
 namespace CustomUtils {
 	class String;
 	template <class T>
@@ -24,6 +23,28 @@ namespace CustomUtils {
 				resize( (len + 1) * sizeof(T) );
 				new (alloc) T(Utils::forward<Ts>(vals)...);
 				place(len, alloc);
+			}
+			Vector& operator =(const UVector& val) {
+				static_assert(Utils::is_constructible<T, const T&>, "type has no copy constructor");
+				UVector::copy(val);
+				return *this;
+			}
+			template <typename V>
+			Vector& operator =(const Vector<V>& val) {
+				static_assert(Utils::is_constructible<T, const V&>, "cannot convert types");
+				copy(val, &Utils::wrap_construct<T, const V&>);
+				return *this;
+			}
+			Vector& operator =(Vector&& val) noexcept {
+				static_assert(Utils::is_assignable<T&, T&&>, "type is not move-constructible");
+				save(val, &Utils::wrap_assign<T&, T&&>);
+				return *this;
+			}
+			template <typename Z>
+			void foreach(Z&& call) {
+				if (trulen > 0)
+					for (Utils::size n = 0; n < len; n++)
+						call(*downcast<T*>(&raw[sizeof(T) * n]));
 			}
 			T& operator[](Utils::size n) {
 				return *downcast<T*>(&raw[sizeof(T) * n]);
