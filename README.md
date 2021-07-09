bab7733bd09919f565c42a42a928fa4fd41ad1f1
Colton Jensen <mystuff20041@gmail.com>
2021-07-08 20:02:26 -0500

    modified readme and modified parent check in .extra/proc/goup

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ A new take on c++ extensions with extra flexibility
 
 PREREQUISITES:
 A machine that runs Unix or Linux
-gcc 8 (gcc 9 or greater preferred but definitely not nessecary)
+bash (version 4.4 or higher)
+gcc 8 or higher (gcc 9 or greater preferred but definitely not nessecary)
 make (this is how it installs)
 
 ## Objective and reasoning:
--- hey&\(.cpp
+++ hey&\(.cpp
@@ -4,6 +4,7 @@
 using namespace CustomUtils;
 
 int main() {
-	std::cout << String("this file is weirdly named") << std::endl;
-	std::cout << String("tabs, spaces, and newlines aren't allowed in test file generations") << std::endl;
+	std::cout << String("this file is weirdly named, but doesn't violate the rules") << std::endl;
+	std::cout << String("tabs, spaces, and newlines aren't allowed in test file names,\n"
+			"as I cannot properly escape them for make") << std::endl;
 }
