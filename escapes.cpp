6d3ae1888dff795f90ae92738ac0c1a84ccd8815
Colton Jensen <mystuff20041@gmail.com>
2021-07-09 20:02:47 -0500

    made ./escapes.o more verbose

--- escapes.cpp
+++ escapes.cpp
@@ -6,6 +6,8 @@ using namespace GString;
 using namespace GString::codes;
 
 int main() {
-	std::cout << escape<bold> << "hey" << escape<reset_all> << std::endl;
+	std::cout << escape<bold> << "this is bold" << escape<reset_all> << std::endl;
 	std::cout << escape<italics> << "this is in italics" << escape<reset_all> << std::endl;
+	std::cout << escape<strikethrough> << "this is striked through" << escape<reset_all> << std::endl;
+	std::cout << escape<bg_red> << "this has a red background" << escape<reset_all> << std::endl;
 }
