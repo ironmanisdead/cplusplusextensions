c514f30755faadfbb22494409e2d9c4af21777d9
Colton Jensen <mystuff20041@gmail.com>
2021-07-20 15:25:17 -0500

    removed test objects

--- libs/TestFunc.cpp
+++
@@ -1,9 +0,0 @@
-#include "headers/TestFunc.hpp"
-DLL_HIDE
-namespace CPPExtensions {
-	DLL_PUBLIC Function<int()> FuncTest() noexcept {
-		int i = 0;
-		return [i] () mutable { return i++; };
-	}
-}
-DLL_RESTORE
--- libs/headers/TestFunc.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Function.hpp"
-DLL_HIDE
-namespace CPPExtensions {
-	DLL_PUBLIC Function<int()> FuncTest() noexcept;
-}
-DLL_RESTORE
