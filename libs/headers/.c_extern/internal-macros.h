4d3381f3e95b3ba919f86587e2bf04b7236298b1
Colton Jensen <mystuff20040@gmail.com>
2021-08-13 12:24:33 -0500

    added INITIALIZER() macro for cross-platform library initialization code (Courtesy of Joe Lowe)

--- libs/headers/.c_extern/internal-macros.h
+++ libs/headers/.c_extern/internal-macros.h
@@ -59,6 +59,28 @@
   #define DEPRECATE(fun, msg) __declspec(deprecated(msg)) fun
  #endif
 #endif
+#ifdef __cplusplus
+	#define INITIALIZER(f) \
+		static void f(void); \
+		struct f##_t_ { f##_t_(void) { f(); } }; static f##_t_ f##_; \
+		static void f(void)
+#elif defined(_MSC_VER)
+	#pragma section(".CRT$XCU",read)
+	#define INITIALIZER2_(f,p) \
+		static void f(void); \
+		__declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
+		__pragma(comment(linker,"/include:" p #f "_")) \
+		static void f(void)
+	#ifdef _WIN64
+		#define INITIALIZER(f) INITIALIZER2_(f,"")
+	#else
+		#define INITIALIZER(f) INITIALIZER2_(f,"_")
+	#endif
+#else
+	#define INITIALIZER(f) \
+	static void f(void) __attribute__((constructor)); \
+	static void f(void)
+#endif
 #ifndef Utils_offset
  #define Utils_offset(type, field) ((decltype(sizeof 0))(&((type*)0)->field))
 #endif
